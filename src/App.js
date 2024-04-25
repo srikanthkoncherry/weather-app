644cabbfae929ab3e50a2198414c72a2992bd11a
= <=>
2024-04-25 12:45:46 -0400

    search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from './components/Search';
+import Weather from './components/Weather';
+import React, { useEffect, useState } from 'react';
+import './styles/Morph.css'
 
 function App() {
+  const [location, setLocation] = useState('')
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    console.log("Loation changes to", location)
+    fetchWeatherData()
+  }, [location])
+  const handleSearch = (city) => {
+    console.log("Handling search for ", city)
+    setLocation(city)
+  }
+
+  async function fetchWeatherData() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+    console.log("searching open api for ", url)
+    try {
+      const response = await fetch(url)
+      console.log(response.json)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cotainer">
+      <Search handleSearch={handleSearch} />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import { Button } from 'react-bootstrap'
+import { AsyncPaginate } from 'react-select-async-paginate'
+
+const Search = ({ handleSearch }) => {
+    const [input, setInput] = useState('')
+    const [options, setOptions] = useState('')
+    const [city, setCity] = useState('')
+
+    const geoOptions = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '8ef1dca0a4msh52e633af1c31ef9p18ea49jsn87263a4321e5',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
+    const cityuri = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=1000000&sort=-population&namePrefix=' + input;
+
+    async function searchOptions() {
+        try {
+            const response = await fetch(cityuri, geoOptions)
+            const result = await response.json()
+            let res = result.data.map(data => data.city)
+            console.log("Rapid api respone = ", res)
+            setOptions(res)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        console.log("INPUT = ", input)
+        if (input !== '') {
+            searchOptions()
+        }
+        console.log("OPTIONS = ", options)
+    }, [input])
+
+    const handleOnChange = (event) => {
+        console.log("In handle on change with ", event.target.value)
+        setInput(event.target.value)
+    }
+
+    const handleOnClick = () => {
+        handleSearch(city)
+        setCity('')
+        setOptions([])
+        setInput('')
+    }
+    return (
+        <div>
+            <div className="input-group mb-3">
+                <input
+                    placeholder='Enter'
+                    type="text"
+                    className="form-control"
+                    value={input}
+                    onChange={handleOnChange}>
+                </input>
+                <span className="input-group-btn">
+                    <Button
+                        className="btn btn-default"
+                        type="button"
+                        onClick={handleOnClick}>
+                        Search
+                    </Button>
+                </span>
+            </div>
+            {(options.length > 0 && city === '') &&
+                <div className="form-control">
+                    {options.map((option) => (
+                        <div
+                            className="litem"
+                            key={option}
+                            onClick={() => {
+                                console.log("SELECTED ", option)
+                                setCity(option)
+                                setInput(option)
+                            }}>
+                            <Button>{option}</Button>
+                        </div>
+                    ))}
+                </div>
+            }
+        </div>
+    );
+}
+
+export default Search;
